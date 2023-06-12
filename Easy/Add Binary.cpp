55ddd4c09aca3c71c0d05620a7644ab43bb000ef
Alexander Kobez <alexander.kobez@gmail.com>
2023-06-12 21:43:03 +0300

    Update Add Binary.cpp

--- Easy/Add Binary.cpp
+++ Easy/Add Binary.cpp
@@ -8,3 +8,14 @@ string addBinary( string a, string b ) {
   if ( i ) a.insert( 0, "1" );
   return a;
 }
+
+#if 0
+string addBinary( string a, string b ) {
+  if ( a == "0" && b == "0" ) return a;
+  bitset< 10000 > ba( a ), bb( b );
+  const size_t s{ max( a.size(), b.size() ) };
+  for ( size_t i{}, n{}; i <= s; ++i )
+    n += ba[ i ] + bb[ i ], ba[ i ] = n & 1, n /= 2;
+  return a = ba.to_string(), a.substr( a.find( '1' ) );
+}
+#endif
