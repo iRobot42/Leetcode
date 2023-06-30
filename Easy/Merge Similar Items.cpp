8c2ec52582953e071f7284c59731afe4a1012be0
Alexander Kobez <alexander.kobez@gmail.com>
2023-06-30 04:18:49 +0300

    Create Merge Similar Items.cpp

---
+++ Easy/Merge Similar Items.cpp
@@ -0,0 +1,9 @@
+vector< vector< int >> mergeSimilarItems( vector< vector< int >>& items1,
+                                          vector< vector< int >>& items2 ) {
+  map< int, int > hash;
+  for ( const auto& item : items1 ) hash[ item[ 0 ] ] += item[ 1 ];
+  for ( const auto& item : items2 ) hash[ item[ 0 ] ] += item[ 1 ];
+  vector< vector< int >> result;
+  for ( const auto [ value, weight ] : hash ) result.push_back({ value, weight });
+  return result;
+}
