6632fc6819f878ccd00137197ebdd9b727bb27aa
Alexander Kobez <alexander.kobez@gmail.com>
2023-07-22 18:59:27 +0300

    Update Find the Index of the First Occurrence in a String.cpp

--- Easy/Find the Index of the First Occurrence in a String.cpp
+++ Easy/Find the Index of the First Occurrence in a String.cpp
@@ -1,6 +1,5 @@
 struct Solution {
-  int strStr( string haystack, string needle ) {
-    const size_t p{ haystack.find( needle ) };
-    return p == string::npos? -1 : p;
-  }
+int strStr( string haystack, string needle ) {
+  return haystack.find( needle );
+}
 };
