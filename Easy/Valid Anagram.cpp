0f09613a551c394e4ca93a14ba65c445c7b1ca61
Alexander Kobez <alexander.kobez@gmail.com>
2024-08-18 00:03:11 +0300

    Update Valid Anagram.cpp

--- Easy/Valid Anagram.cpp
+++ Easy/Valid Anagram.cpp
@@ -2,7 +2,6 @@ bool isAnagram( string s, string t ) {
   int a[ 26 ]{};
   for ( const char c : s ) ++a[ c-'a' ];
   for ( const char c : t ) --a[ c-'a' ];
-  for ( int i{}; i < 26; ++i )
-    if ( a[ i ] ) return false;
+  for ( const int i : a ) if ( i ) return false;
   return true;
 }
